e2439574b32da00a06d886e8a15b9ea377bf24e0
Yan <yan71884@gmail.com>
2021-05-14 16:35:33 +0300

    Feat: complete 1 task

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,26 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+
+    NSMutableArray *new = [[NSMutableArray alloc] init];
+    
+    while (n > 0) {
+        int r = n % 2; // r = 0
+        n /= 2; // n = 1
+        [new insertObject:@(r) atIndex:0];
+    }
+
+    long i = [new count];
+    
+    do {
+        [new insertObject:@(0) atIndex:0];
+        i++;
+    } while (i < 8);
+    
+    NSArray *reverse = [[new reverseObjectEnumerator] allObjects];
+    NSString *reverseString = [reverse componentsJoinedByString:@""];
+    long c = strtol([reverseString UTF8String], NULL, 2);
+    
+    return c;
+    
 }
