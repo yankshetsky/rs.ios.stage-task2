3dd524d1d2aed361d855050efd07754b39e3991a
Yan <yan71884@gmail.com>
2021-05-15 13:18:39 +0300

    Add: task 4 in progress

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,62 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+    NSMutableArray *digits = [[NSMutableArray alloc] init];
+    NSMutableArray *strings = [[NSMutableArray alloc] init];
+    
+    for (int a = 0; a < [array count]; a++) {
+        NSMutableArray *check = [NSMutableArray arrayWithArray: [array objectAtIndex:a]];
+        
+        bool ravno = true;
+        
+        for (int b = 1; b < [check count]; b++) {
+            
+            if ([[check objectAtIndex: 0] isKindOfClass: [NSString class]]) {
+                if ([[check objectAtIndex: b] isKindOfClass: [NSString class]]) {
+                    continue;
+                } else {
+                    ravno = false;
+                }
+            } else {
+                if ([[check objectAtIndex: b] isKindOfClass: [NSNumber class]]) {
+                    continue;
+                } else {
+                    ravno = false;
+                }
+            }
+        }
+        
+                                
+        if (ravno) {
+            for (int b = 0; b < [check count]; b++) {
+                NSString *el = [check objectAtIndex:b];
+                if ([el isKindOfClass:NSNumber.class]) {
+                    [digits addObject: el];
+                } else {
+                    [strings addObject: el];
+                }
+            }
+        }
+    }
+    
+    NSArray *sortedStrings = [strings sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+
+    NSSortDescriptor* sortOrder = [NSSortDescriptor sortDescriptorWithKey: @"self"
+                                                                ascending: YES];
+    NSArray *sortedDigits = [digits sortedArrayUsingDescriptors: [NSArray arrayWithObject: sortOrder]];
+    
+    NSArray *final = [[NSArray alloc] init];
+    
+    if ([sortedStrings count] != 0 && [sortedDigits count] == 0) {
+        final = sortedStrings;
+    } else if ([sortedStrings count] == 0 && [sortedDigits count] != 0) {
+        final = sortedDigits;
+    } else {
+        final = @[];
+    }
+    
+    return final;
 }
 
 @end
