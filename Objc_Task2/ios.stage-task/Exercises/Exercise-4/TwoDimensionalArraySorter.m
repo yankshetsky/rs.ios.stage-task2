ca960708d89f3047e61bb8e01eb0a05352e3a1ba
Yan <yan71884@gmail.com>
2021-05-15 16:09:34 +0300

    Feat: complete 4 task

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -6,48 +6,72 @@
     
     NSMutableArray *digits = [[NSMutableArray alloc] init];
     NSMutableArray *strings = [[NSMutableArray alloc] init];
+    bool ravno = true;
     
-    for (int a = 0; a < [array count]; a++) {
-        NSMutableArray *check = [NSMutableArray arrayWithArray: [array objectAtIndex:a]];
-        
-        bool ravno = true;
-        
-        for (int b = 1; b < [check count]; b++) {
+    if ([[array objectAtIndex: 0] isKindOfClass: [NSArray class]]) {
+        for (int i = 0; i < [array count]; i++) {
+            NSMutableArray *check = [NSMutableArray arrayWithArray: [array objectAtIndex: i]];
             
-            if ([[check objectAtIndex: 0] isKindOfClass: [NSString class]]) {
-                if ([[check objectAtIndex: b] isKindOfClass: [NSString class]]) {
+            for (int b = 1; b < [check count]; b++) {
+                
+                if ([[check objectAtIndex: 0] isKindOfClass: [NSString class]]) {
+                    if ([[check objectAtIndex: b] isKindOfClass: [NSString class]]) {
+                        continue;
+                    } else {
+                        ravno = false;
+                    }
+                } else {
+                    if ([[check objectAtIndex: b] isKindOfClass: [NSNumber class]]) {
+                        continue;
+                    } else {
+                        ravno = false;
+                    }
+                }
+            }
+            if (ravno) {
+                for (int b = 0; b < [check count]; b++) {
+                    NSString *el = [check objectAtIndex:b];
+                    if ([el isKindOfClass:NSNumber.class]) {
+                        [digits addObject: el];
+                    } else {
+                        [strings addObject: el];
+                    }
+                }
+            }
+        }
+    }
+    else {
+        for (int i = 1; i < [array count]; i++) {
+            if ([[array objectAtIndex: 0] isKindOfClass: [NSString class]]) {
+                if ([[array objectAtIndex: i] isKindOfClass: [NSString class]]) {
                     continue;
                 } else {
                     ravno = false;
                 }
             } else {
-                if ([[check objectAtIndex: b] isKindOfClass: [NSNumber class]]) {
+                if ([[array objectAtIndex: i] isKindOfClass: [NSNumber class]]) {
                     continue;
                 } else {
                     ravno = false;
                 }
             }
         }
-        
-                                
-        if (ravno) {
-            for (int b = 0; b < [check count]; b++) {
-                NSString *el = [check objectAtIndex:b];
-                if ([el isKindOfClass:NSNumber.class]) {
-                    [digits addObject: el];
-                } else {
-                    [strings addObject: el];
+            if (ravno) {
+                for (int b = 0; b < [array count]; b++) {
+                    NSString *el = [array objectAtIndex: b];
+                    if ([el isKindOfClass:NSNumber.class]) {
+                        [digits addObject: el];
+                    } else {
+                        [strings addObject: el];
+                    }
                 }
             }
-        }
     }
     
     NSArray *sortedStrings = [strings sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
-
     NSSortDescriptor* sortOrder = [NSSortDescriptor sortDescriptorWithKey: @"self"
                                                                 ascending: YES];
     NSArray *sortedDigits = [digits sortedArrayUsingDescriptors: [NSArray arrayWithObject: sortOrder]];
-    
     NSArray *final = [[NSArray alloc] init];
     
     if ([sortedStrings count] != 0 && [sortedDigits count] == 0) {
@@ -57,6 +81,8 @@
     } else {
         final = @[];
     }
+    
+    NSLog(@"%@", final);
     
     return final;
 }
